dea66a156515e702daa5dbb053b790b51fba8559
dcpersons <danielcpersons@gmail.com>
2026-06-12 18:27:45 -0400

    Add keyboard functionality
    Misc bug fixes

--- calculator.js
+++ calculator.js
@@ -1,65 +1,63 @@
 const screen = document.querySelector(".screen");
-const buttons = document.querySelectorAll("button")
+const digits = document.querySelectorAll(".digits button");
+const operators = document.querySelectorAll(".operators button");
 let firstNum = '';
 let secondNum = '';
 let operator = '=';
 let result = ''
-// if (operator = '='):
-// firstnum += "button clicked"
-// if button clicked = a sign
-// set operator variable and 
-// if secondnum = '' : begin assigning second number
-// if secondnum != '' : run operate function 
-// firstnum = result of operate
-// secondnum = ''
-// operator = sign clicked
 
-buttons.forEach(button => {
-    button.addEventListener("click", (event) => {
-        const clicked = (event.srcElement.className);
-        // if clear is clicked, clear 1st 2nd and result, set operator to =
-        if (clicked == "clear") {
-            result = '';
-            firstNum = '';
-            secondNum = '';
-            operator = '=';
-            screen.textContent = '';
-        }
-        // if = is clicked, operate and set result. set first and second num to '' and operator to =
-        else if (clicked == '=') {
-            if (firstNum == '') result = operate(result, secondNum, operator);
-            else result = operate(firstNum, secondNum, operator);
-            firstNum = '';
-            secondNum = '';
-            operator = '=';
-        }
-        // assign first number until an operator is assigned
-        else if ((Number(clicked) || clicked == 0) && operator == '=') {
-            firstNum += clicked;
-            screen.textContent = firstNum;
-        }
-        // assign second number when operator is assigned
-        else if ((Number(clicked) || clicked == 0)) {
-            secondNum += clicked;
-            screen.textContent = `${firstNum} ${operator} ${secondNum}`
-        }
-        // if a sign is clicked with no second number, set operator variable
-        else if (!(Number(clicked)) && operator == '=') {
-            operator = clicked;
-            if (firstNum == '') firstNum = result;
-            screen.textContent = `${firstNum} ${operator}`;
-        }
-        // if a sign is clicked with a second number, operate and set firstnum to result
-        else if (!Number(clicked)) {
-            result = operate(firstNum, secondNum, operator);
-            firstNum = result
-            secondNum = '';
-            operator = clicked;
-            screen.textContent = `${firstNum} ${operator}`;
-        }
-        console.log(clicked);
-    })
-});
+function printDigit(digit) {
+    // assign first number until an operator is assigned
+    if (operator == '=') {
+        (digit == '.' && firstNum.includes('.')) ? null :
+        firstNum += digit
+        screen.textContent = firstNum;
+    }
+    
+    // assign second number when operator is assigned
+    else {
+    (digit == '.' && secondNum.includes('.'))  ? null :
+    secondNum += digit
+    screen.textContent = `${firstNum} ${operator} ${secondNum}`
+    }
+}
+
+function printOperator(sign) {
+    if (sign == "clear") {
+        result = '';
+        firstNum = '';
+        secondNum = '';
+        operator = '=';
+        screen.textContent = '';
+    }
+    // if = is clicked, operate and set result. set first and second num to '' and operator to =
+    else if (sign == '=') {
+        if (firstNum === '') firstNum = result;
+        if (secondNum === '') result = operate(firstNum, secondNum, "=")
+        result = operate(firstNum, secondNum, operator);
+        firstNum = '';
+        secondNum = '';
+        operator = '=';
+    }
+
+    // if a sign is clicked with no second number, set operator variable
+    else if (secondNum === '') {
+        if (firstNum === '') firstNum = result;
+        if (firstNum === '' && sign !== '-') null;
+        else operator = sign, screen.textContent = `${firstNum} ${operator}`;
+    } 
+
+    // if a sign is clicked with a second number, operate and set firstnum to result
+    else if (secondNum !== '') {
+        result = operate(firstNum, secondNum, operator);
+        firstNum = result;
+        secondNum = '';
+        operator = sign;
+        screen.textContent = `${firstNum} ${operator}`;
+    }
+    
+    else operator = sign, screen.textContent = `${firstNum} ${operator}`;
+}
 
 
 function operate(a, b, sign){
@@ -74,7 +72,7 @@ function operate(a, b, sign){
             screen.textContent = Math.floor((+a * +b) * 10000) / 10000;
             return `${Math.floor((+a * +b) * 10000) / 10000}`;
         case "\u00F7":
-            if (b == 0) {screen.textContent = "I can't believe you've done this"; return 0}
+            if (b == 0) {screen.textContent = "I can't believe you've done this"; return '0'}
             else {screen.textContent = Math.floor((+a / +b) * 10000) / 10000;
             return `${Math.floor((+a / +b) * 10000) / 10000}`};
         case "=":
@@ -83,3 +81,28 @@ function operate(a, b, sign){
         default: screen.textContent = "ERROR"; 
     }
 }
+
+digits.forEach(button => {
+    button.addEventListener("click", (event) => {
+        const digit = (event.srcElement.className);
+        printDigit(digit);
+    })
+})
+
+
+operators.forEach(button => {
+    button.addEventListener("click", (event) => {
+        const clicked = (event.srcElement.className);
+        printOperator(clicked);
+    })
+})
+
+document.addEventListener("keydown", (event) => {
+    const key = String(event.key);
+    if (key >= 0 && key <= 9 || key == '.') printDigit(key);
+    if (key == '+' || key == '-' || key == '*' || key == '=') printOperator(key);
+    if (key == '/') printOperator('\u00F7');
+    if (key == 'Enter') printOperator('=');
+    if (key == 'Delete' || key == 'Backspace') printOperator('clear');
+     console.log(event)
+})
